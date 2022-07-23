54a48075219875f442d341e6f93cc2265f2e6115
Yasir Jafar <yasir991925@gmail.com>
2022-07-23 17:21:23 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+import Eventbus from "@vertx/eventbus-bridge-client.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [eb, setEb] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [text, setText] = useState("");
+    const mesRef = useRef([]);
+
+    useEffect(() => {
+        const eb = new Eventbus("/eventbus");
+        eb.enableReconnect(true);
+        setEb(eb);
+
+        if (eb) {
+            eb.onopen = function () {
+                eb.registerHandler("msg", (err, msg) => {
+                    mesRef.current = [...mesRef.current, JSON.parse(msg.body)];
+                    if (!err) setMessages([...mesRef.current]);
+                });
+            };
+        }
+
+        console.log("render");
+    }, []);
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+    };
+
+    const renderMessages = () => {
+        return messages.sort((a, b) => b.timestamp - a.timestamp).map((m, i) => (
+            <div key={i} className="mbox">
+				<div>{m.server}</div>
+				<div>{m.message}</div>
+				<div>{m.timestamp}</div>
+            </div>
+        ));
+    };
+
+    const sendMessage = () => {
+        if (eb) {
+            eb.send("msg-back", text);
+            setText("");
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>Scalling web sockets</h1>
+            <form onSubmit={(e) => e.preventDefault()}>
+                <input onChange={handleChange} value={text} />
+                <button onClick={sendMessage}>Send</button>
+                <br />
+            </form>
+            <div className="container">{renderMessages()}</div>
+        </div>
+    );
 }
 
 export default App;
